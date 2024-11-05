5fedf40f30aeb77168f2c2b0b1db1181bf3e0411
Yash <singhyash0026@gmail.com>
2024-11-05 11:25:07 +0530

    object-02

--- 08-objects/objects.js
+++ 08-objects/objects.js
@@ -1,6 +1,31 @@
 // Object Literals
 
+const symbol = Symbol('key1');
+
 let info = {
     name:"Max",
+    age: 21,
+    [symbol]: 'keys' 
 }
 
+console.log(info);
+console.log(info.age);
+console.log(info.name);
+
+console.log(info['name']);
+console.log(info['age']);
+
+// objects properties,
+info.name = "John";
+console.log(info);
+// Object.freeze(info);
+// once the object is freezed, nothing will change.
+info.name = "max"
+console.log(info)
+
+info.display = ()=>{
+    console.log("Hello");
+}
+
+console.log(info['name']);
+console.log(info.display);

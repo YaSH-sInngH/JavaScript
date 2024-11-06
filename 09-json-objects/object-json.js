f63bf5521e9b12bf52bc679dec50bbc190b0bdf3
Yash <singhyash0026@gmail.com>
2024-11-06 23:06:08 +0530

    json

---
+++ 09-json-objects/object-json.js
@@ -0,0 +1,17 @@
+// This is json format which is mainly focuses on api calling and fetching data etc.
+// The below format is json or object format for API.
+// {
+//     "name":'max',
+//     "age": 21,
+//     "gender": 'male'
+// }
+
+// The below format is array format for API.
+// [
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+// ]
